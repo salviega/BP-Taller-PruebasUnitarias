642fb7fd2a6f4c966be313329b41b51c3332bf9a
Santiago Viana <salviega6@gmail.com>
2022-05-11 07:40:10 -0500

    cache eliminado

--- README.md
+++
@@ -1,3 +0,0 @@
-# BP-Práctica-Pruebas Unitarias con Maiven
-
-Buenas Prácticas: Práctica - Pruebas Unitarias con Maiven.
--- src/main/java/SubDependency.java
+++
@@ -1,9 +0,0 @@
-
-public class SubDependency {
-    public  SubDependency() {
-
-    }
-    public String getClassName() {
-        return this.getClass().getSimpleName();
-    }
-}
